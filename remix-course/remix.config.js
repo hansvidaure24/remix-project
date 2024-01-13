b7ce0386213e83ffad91bdaef90e82fbf5ac6356
Hans <hansvidaure24@gmail.com>
2024-01-12 19:18:43 -0500

    config

--- remix-course/remix.config.js
+++ remix-course/remix.config.js
@@ -5,4 +5,12 @@ export default {
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
+  future: {
+    v2_dev: true,
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  }
 };
